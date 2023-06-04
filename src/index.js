dec0a66164ffe6ad150dc4af9635cdf86c4d653f
mempoolnode-bt <135513207+mempoolnode-bt@users.noreply.github.com>
2023-06-04 19:01:01 +0100

    Updated tsconfigs

--- src/index.js
+++ src/index.js
@@ -1,33 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectionManager = void 0;
-var ws_1 = require("ws");
-var connectionManager = function (apiKey, url) {
-    if (url === void 0) { url = null; }
+const ws_1 = require("ws");
+const connectionManager = (apiKey, url = null) => {
     if (url != null && url.endsWith("/")) {
         url = url.slice(0, -1);
     }
-    var connectionUrl = url == null ? "wss://api.mempoolnode.com/ws?apiKey=".concat(apiKey) : "".concat(url, "?apiKey=").concat(apiKey);
+    const connectionUrl = url == null ? `wss://api.mempoolnode.com/ws?apiKey=${apiKey}` : `${url}?apiKey=${apiKey}`;
     var socket = null;
     var onMessageHandler;
     var onCloseHandler;
     var onErrorHandler;
     var registered = false;
-    var registerHandlers = function (callback, onError) {
-        if (onError === void 0) { onError = console.error; }
+    const registerHandlers = (callback, onError = console.error) => {
         if (registered) {
             onError('A callback handler has already been registered. Call .stop() first.');
             return;
         }
         socket = new ws_1.WebSocket(connectionUrl);
-        onMessageHandler = function (ev) { return callback(ev.data); };
-        onCloseHandler = function (ev) {
-            onError("Server closed conection: \"".concat(JSON.stringify(ev), "\". Please remediate before calling .start() again."));
+        onMessageHandler = (ev) => callback(ev.data);
+        onCloseHandler = (ev) => {
+            onError(`Server closed conection: "${JSON.stringify(ev)}". Please remediate before calling .start() again.`);
             registered = false;
         };
-        onErrorHandler = function (ev) {
-            onError("Error occurred \"".concat(ev.message, "\". Retrying in 1s"));
-            setTimeout(function () {
+        onErrorHandler = (ev) => {
+            onError(`Error occurred "${ev.message}". Retrying in 1s`);
+            setTimeout(() => {
                 socket === null || socket === void 0 ? void 0 : socket.removeEventListener('message', onMessageHandler);
                 socket === null || socket === void 0 ? void 0 : socket.removeEventListener('close', onCloseHandler);
                 socket === null || socket === void 0 ? void 0 : socket.removeEventListener('error', onErrorHandler);
@@ -36,7 +34,7 @@ var connectionManager = function (apiKey, url) {
                 register(socket);
             }, 1000);
         };
-        var register = function (ws) {
+        const register = (ws) => {
             console.log('Starting new connection with MempoolNode');
             ws.addEventListener('message', onMessageHandler);
             ws.addEventListener('close', onCloseHandler);
@@ -46,11 +44,10 @@ var connectionManager = function (apiKey, url) {
         register(socket);
     };
     return {
-        start: function (callback, onError) {
-            if (onError === void 0) { onError = console.error; }
+        start: (callback, onError = console.error) => {
             registerHandlers(callback, onError);
         },
-        stop: function () {
+        stop: () => {
             socket === null || socket === void 0 ? void 0 : socket.removeEventListener('message', onMessageHandler);
             socket === null || socket === void 0 ? void 0 : socket.removeEventListener('close', onCloseHandler);
             socket === null || socket === void 0 ? void 0 : socket.removeEventListener('error', onErrorHandler);
@@ -60,3 +57,4 @@ var connectionManager = function (apiKey, url) {
     };
 };
 exports.connectionManager = connectionManager;
+//# sourceMappingURL=index.js.map
